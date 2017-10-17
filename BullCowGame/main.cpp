02df624776e034455c51b61c446db06066e11043
abcarlisle <aaronbcarlisle@gmail.com>
2017-10-17 19:06:12 -0400

    Updating intro message.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
-// name spaces
+// namespaces
 using namespace std;
 
 int main()
 {
-	cout << "Welcome to Bulls and Cows" << endl;
+	// using magic number
+	constexpr int WORD_LENGTH = 8;
+
+	// intro
+	cout << "Welcome to Bulls and Cows, a fun word game\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+    cout << " letter isogram I'm thinking of?\n";
+
+	// get a guess from the player
+	string Guess = "";
+
+	// repeat the guess back to them
+
 	return 0;
 }
