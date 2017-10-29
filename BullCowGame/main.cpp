65aaf530be94268ccdefe1e04b1d38b946f63787
Aaron Carlisle <aaronbcarlisle@gmail.com>
2017-10-28 20:05:46 -0400

    Linux doesn't care to much for constexp types

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -14,77 +14,77 @@ FBullCowGame BCGame;
 
 int main()
 {
-	// play game 
-	bool bPlayAgain = false;
-	do {
-		PrintIntro();
-		PlayGame();
-		bPlayAgain = AskToPlayAgain();
-	} 
-	while (bPlayAgain);
+    // play game
+    bool bPlayAgain = false;
+    do {
+        PrintIntro();
+        PlayGame();
+        bPlayAgain = AskToPlayAgain();
+    }
+    while (bPlayAgain);
 
-	// exit application
-	return 0;
+    // exit application
+    return 0;
 }
 
 void PrintIntro()
 {
-	// magic number for word length
-	constexpr int WORD_LENGTH = 8;
+    // magic number for word length
+    const int WORD_LENGTH = 8;
 
-	// intro
-	std::cout << "Welcome to Bulls and Cows, a fun word game\n";
-	std::cout << "Can you guess the " << WORD_LENGTH;
-	std::cout << " letter isogram I'm thinking of?\n";
-	std::cout << std::endl;
+    // intro
+    std::cout << "Welcome to Bulls and Cows, a fun word game\n";
+    std::cout << "Can you guess the " << WORD_LENGTH;
+    std::cout << " letter isogram I'm thinking of?\n";
+    std::cout << std::endl;
 }
 
 void PlayGame()
 {
 
-	// get max tries
-	int MaxTries = BCGame.GetMaxTries();
+    // get max tries
+    int MaxTries = BCGame.GetMaxTries();
 
-	// loop through turns.
-	for (int count = 1; count <= MaxTries; count++)
-	{
-		// get guess and print back
-		PrintGuess(GetGuess());
-	}
+    // loop through turns.
+    for (int count = 1; count <= MaxTries; count++)
+    {
+        // get guess and print back
+        PrintGuess(GetGuess());
+    }
 }
 
-std::string GetGuess() 
+std::string GetGuess()
 {
-	// current try
-	int CurrentTry = BCGame.GetCurrentTry();
+    // current try
+    int CurrentTry = BCGame.GetCurrentTry();
 
-	// ask for guess
-	std::string Guess = "";
-	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+    // ask for guess
+    std::string Guess = "";
+    std::cout << "Try " << CurrentTry << ". Enter your guess: ";
 
-	// get guess
-	std::getline(std::cin, Guess);
+    // get guess
+    std::getline(std::cin, Guess);
 
-	return Guess;
+    return Guess;
 }
 
 void PrintGuess(std::string Guess)
 {
-	// repeat the guess back
-	std::cout << "Your guess was: " << Guess << std::endl;
+    // repeat the guess back
+    std::cout << "Your guess was: " << Guess << std::endl;
     std::cout << std::endl;
 }
 
 bool AskToPlayAgain()
 {
-	// ask to play again
-	std::string Response = "";
-	std::cout << "Do you want to play again (y/n)? ";
+    // ask to play again
+    std::string Response = "";
+    std::cout << "Do you want to play again (y/n)? ";
 
-	// get response
-	std::getline(std::cin, Response);
+    // get response
+    std::getline(std::cin, Response);
     std::cout << std::endl;
 
-	// return bool (y for true, anything else for false)
-	return (Response[0] == 'y') || (Response[0] == 'Y');
+    // return bool (y for true, anything else for false)
+    return (Response[0] == 'y') || (Response[0] == 'Y');
 }
